c07a645161bf57b5b832b57e9b5c76b40c1b9b37
Nate <43708919+nrb5089@users.noreply.github.com>
2018-10-21 17:02:56 -0400

    Update util.py

--- util.py
+++ util.py
@@ -3,52 +3,16 @@ This is a utility file that does data processing
 """
 
 import numpy as np
-
+from PIL import Image
+import os
+import cv2
 
 class DataProcessing:
-
+    
     def __init__(self):
         pass
     
-    def preprocess(self, data, proc_type):
-        """
-
-        :param data: a numpy array of size M,N
-        :param proc_type: the pre-processing type to be done on the image
-        :return: the preprocessed data
-        """
-        
-    def random_crops(path,savepath,numpatches,Mdim,Ndim):
-        '''
-        This function generates numpatches number random cropped patches of size 
-        Mdim X Ndim.
-        params: path: path which the images are loaded
-                savepath: path which the cropped patches are saved
-                numpatches: number of cropped patches per photo
-                Mdim: M dimension patch length
-                Ndim: N dimension patch width
-        returns: Nothing
-        '''
-
-        files = os.listdir(path)
-        for file in files:
-            fullpath = os.path.join(path,file)         
-            if os.path.isfile(fullpath):
-                im = Image.open(fullpath)
-                f, e = os.path.splitext(file)
-                (M,N) = im.size
-                rnglim = (M-Mdim,N-Ndim)
-                shufidxm = np.random.choice(np.arange(0,rnglim[0]),size = numpatch, replace = False)
-                shufidxn = np.random.choice(np.arange(0,rnglim[1]),size = numpatch, replace = False)
-                for jj in range(0,numpatch):
-                    cropm = shufidxm[jj]
-                    cropn = shufidxn[jj]
-                    imCrop = im.crop((cropm, cropn, cropm + 120, cropn + 120)) #corrected
-    #                imNoise = np.array(imCrop) + 10*np.random.randn(180,180,3)
-    #                imCrop= Image.fromarray(np.uint8(imNoise/255))
-                    imCrop.save(savepath + f + str(jj) +'.bmp', "BMP", quality=100)
-        
-    def resize(path,savepath):
+    def resize(self,path,savepath):
         '''
         This function loads all images from path and resizes them to 256x256, then
         the files are saved as .bmp in as the new size in savepath.  This function 
@@ -58,6 +22,7 @@ class DataProcessing:
         Returns: Nothing
         '''
         files = os.listdir(path)
+        print('Resizing...')
         for file in files:
             fullpath = os.path.join(path,file)         
             if os.path.isfile(fullpath):
@@ -65,70 +30,103 @@ class DataProcessing:
                 f, e = os.path.splitext(file)
                 imResized = im.resize((256,256)) 
                 imResized.save(savepath + f + '.bmp', "BMP", quality=100)
-
-    def load_images(self, path):
-        """
-
-        :param path: the location to read the images, ensure end of path is appended with "\\"
-        :return     X: 4D numpy array of images where each ith index (i,M,N,3) is an RGB image M by N.
-                files: list of filenames that each ith entry corresponds to the ith image in X.
-        """
+                
+    def random_crops(self,path,savepath,numpatches,Mdim,Ndim):
+        '''
+        This function generates numpatches number random cropped patches of size 
+        Mdim X Ndim. Saves to savepath.
+        params: path: path which the images are loaded
+                savepath: path which the cropped patches are saved
+                numpatches: number of cropped patches per photo
+                Mdim: M dimension patch length
+                Ndim: N dimension patch width
+        returns: Nothing
+        '''
+        
+        files = os.listdir(path)
+        print('Generating random patches...')
+        for file in files:
+            fullpath = os.path.join(path,file)         
+            if os.path.isfile(fullpath):
+                im = Image.open(fullpath)
+                f, e = os.path.splitext(file)
+                (M,N) = im.size
+                rnglim = (M-Mdim,N-Ndim)
+                shufidxm = np.random.choice(np.arange(0,rnglim[0]),size = numpatches, replace = False)
+                shufidxn = np.random.choice(np.arange(0,rnglim[1]),size = numpatches, replace = False)
+                for jj in range(0,numpatches):
+                    cropm = shufidxm[jj]
+                    cropn = shufidxn[jj]
+                    imCrop = im.crop((cropm, cropn, cropm + Mdim, cropn + Ndim)) #corrected
+                    imCrop.save(savepath + f + str(jj) +'.bmp', "BMP", quality=100)
+                    
+    def load_images_to_array(self,path):
         images = []
         files = os.listdir(path)
+        print('Reading Files...')
         for filename in files:
             img = cv2.imread(os.path.join(path,filename))
+          
             if img is not None:
-            images.append(img)
+                images.append(img)
         (M,N,C) = img.shape
         X = np.full((len(images),M,N,C),0)
+        print('Translating to Array...')
         for kk in range(0,len(images)):
             X[kk,:,:,:]=images[kk]
+        print('Done Translating')
+            
         return X,files
     
-    def gen_labels(files):
+    def gen_labels(self,files):
         '''
         This function generates labels based on the filenames in the FG-Net dataset
-        :param files: list of filenames to be converted into labels
-        :return pnums: array that identifies which person image i is of
-                 ages: array that identfies which age image i is
+        files: list of filenames to be converted into labels
+        returns pnums: array that identifies which person image i is of
+                ages: array that identfies which age image i is
         '''
         L = len(files)
         pnums = np.zeros(L)
         ages = np.zeros(L)
-        for ii in range(0,L):
+        print('Generating Labels...')
+        for ii in range( 0,L):
             file = str(files[ii])
             pnums[ii] = int(file[0:3])
             ages[ii] = int(file[4:6])
+        print('Labels Generated')
         return pnums,ages
     
-    def label_age_bins(Y,agemax,agemin):
+    
+    def label_age_bins(self,Y,agemax,agemin):
         '''
         This function takes two vectors that specify the min and max for the ith
         age bin, and then generates those labels for each photo based on the actual
         ages **DO NOT OVERLAP BINS**
-        :params     Y: np array of ages of each ith individual
-               agemax: max age of bin i
-               agemin: min age of bin i
-        :returns Y: labels in bins
+        Y: np array of ages of each ith individual
+        agemax: max age of bin i
+        agemin: min age of bin i
+        returns: Y: labels in bins
         '''
-
+        print('Assigning Age Bins...')
+        ages = Y
         for jj in range(0,len(agemax)):
             Y[(ages>=agemin[jj])*(ages<=agemax[jj])]= jj
+        print('Done Binning')
         return Y
-    
-    def shuffle_data(X,Y):
+       
+    def shuffle_data(self,X,Y):
         '''
         Takes both the data and labels and shuffles them parallel
-        :params X: images
-                Y: labels
-        :returns    Xshuff: X shuffled
-                    Yshuff: Y shuffled
+        X: images
+        Y: labels
+        returns:
+            Xshuff: X shuffled
+            Yshuff: Y shuffled
         '''
+        print('Shuffling Data...')
         shufidx = np.random.choice(np.arange(0,len(Y)),size = len(Y), replace = False)
         Xshuff = X[shufidx]
         Yshuff = Y[shufidx]
         return Xshuff, Yshuff
-    
-
 
 
